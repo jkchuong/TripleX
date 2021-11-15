98768c9eca8b420105f52e4d5cb42b6294ad2105
Jimmy <jkchuong@gmail.com>
2021-11-15 13:20:27 +0000

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/TripleX.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TripleX.dir/main.cpp.obj"
+  "TripleX.exe"
+  "TripleX.exe.manifest"
+  "TripleX.lib"
+  "TripleX.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TripleX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
